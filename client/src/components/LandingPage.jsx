41bdcb260df7c85f29b11d33616b3daf68daff06
Gautam2503 <gautamnaggn357@gmail.com>
2024-02-14 12:47:57 +0530

    Changed the Landing Page

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,6 +1,20 @@
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import { MeetingPage } from "./MeetingPage";
 
 export function LandingPage() {
-    return <div>
-        Hi welcome to landing page
-    </div>
+    const navigate = useNavigate();
+
+    const joined = () => {
+        navigate("/meeting/123");
+    };
+
+    return (
+        <div>
+            Hi, welcome to the landing page
+            <button onClick={joined}>Join Meeting</button>
+            <Routes>
+                <Route path="/meeting/:meetingId" element={<MeetingPage />} />
+            </Routes>
+        </div>
+    );
 }
